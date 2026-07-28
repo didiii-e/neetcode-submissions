3e677e85bef466e3e18ac7bfa9d7421657ca5808
didiii-e <112665624+didiii-e@users.noreply.github.com>
2026-07-27 20:27:59 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,41 @@
+class MinStack:
+
+    def __init__(self):
+        self.minstack = []
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        
+        if self.stack:
+            if val < self.minstack[-1]:
+                self.minstack.append(val)
+                self.stack.append(val)
+            else:
+                self.minstack.append(self.minstack[-1])
+                self.stack.append(val)
+        else:
+            self.stack.append(val)
+            self.minstack.append(val)
+
+    def pop(self) -> None:
+        if self:
+            self.stack.pop()
+            self.minstack.pop()
+        else:
+            print('Stack empty -- no value to pop')
+        
+
+    def top(self) -> int:
+        if self:
+            return self.stack[-1]
+        else:
+            print('Stack empty -- no top value')
+        
+
+    def getMin(self) -> int:
+        if self:
+            return self.minstack[-1]
+        else: 
+            print('Stack empty -- no min value')
+        
